f35f3a5cc04e31a16a0e9e389df1b737e5cc6959
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-01 15:25:19 -0300

    ♻️Allow multiple polygon colliders in Room

--- Assets/Scripts/Map/Room.cs
+++ Assets/Scripts/Map/Room.cs
@@ -1,17 +1,24 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
     PolygonCollider2D roomCollider;
-    RoomColliderPoints roomLimits;
+
+    List<RoomColliderPoints> roomLimitsList;
 
     // public Transform position; // For debug, if necessary
 
     void Awake()
     {
-        roomCollider = this.transform.Find("RoomCollider").GetComponent<PolygonCollider2D>();
-        roomLimits = new RoomColliderPoints(roomCollider.points);
+        roomLimitsList = new List<RoomColliderPoints>();
+
+        PolygonCollider2D[] colliders = this.transform.Find("RoomCollider").GetComponents<PolygonCollider2D>();
+        foreach(PolygonCollider2D collider in colliders)
+        {
+            roomLimitsList.Add(new RoomColliderPoints(collider.points));
+        }
     }
 
     void Update()
@@ -22,42 +29,48 @@ public class Room : MonoBehaviour
     // https://demonstrations.wolfram.com/AnEfficientTestForAPointToBeInAConvexPolygon/
     public bool IsPointInsideConvexPolygon(Vector2 point)
     {
-        int numberOfPoints = roomLimits.crude.Length;
+        bool result = false;
 
-        for(int i=0; i<numberOfPoints; i++)
+        foreach(RoomColliderPoints roomPoints in roomLimitsList)
         {
-            // Guarantees the original state
-            roomLimits.trans[i] = roomLimits.crude[i];
+            int numberOfPoints = roomPoints.crude.Length;
 
-            // Translate all polygon points considering point as origin
-            roomLimits.trans[i] = roomLimits.crude[i] - point;
+            for(int i=0; i<numberOfPoints; i++)
+            {
+                // Guarantees the original state
+                roomPoints.trans[i] = roomPoints.crude[i];
 
-            // Translate all polygon points considering gameObject position
-            roomLimits.trans[i] += ((Vector2)this.transform.position);
+                // Translate all polygon points considering point as origin
+                roomPoints.trans[i] = roomPoints.crude[i] - point;
+
+                // Translate all polygon points considering gameObject position
+                roomPoints.trans[i] += ((Vector2)this.transform.position);
+            }
+
+            // Calculate angle between two points and check signs
+            bool allPositive = true;
+            bool allNegative = true;
+
+            for(int i=0; i<numberOfPoints; i++)
+            {
+                Vector3 pointI = roomPoints.trans[i];
+                Vector3 pointJ = roomPoints.trans[(i+1)%numberOfPoints];
+
+                float operation = pointJ.x*pointI.y - pointI.x*pointJ.y;
+                bool angleSignPositive = operation > 0;
+
+                // { // DEBUG ///////////////////
+                //     Debug.Log($"Angle {pointI} {pointJ} - op {operation} - positive {angleSignPositive}");
+                //     Debug.DrawLine(pointI, pointI, Color.magenta, 0.1f);
+                //     GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = pointI;
+                // } // DEBUG ///////////////////
+
+                allPositive &= angleSignPositive;
+                allNegative &= !angleSignPositive;
+            }
+
+            result |= allPositive || allNegative;
         }
-
-        // Calculate angle between two points and check signs
-        bool allPositive = true;
-        bool allNegative = true;
-
-        for(int i=0; i<numberOfPoints; i++)
-        {
-            Vector3 pointI = roomLimits.trans[i];
-            Vector3 pointJ = roomLimits.trans[(i+1)%numberOfPoints];
-
-            float operation = pointJ.x*pointI.y - pointI.x*pointJ.y;
-            bool angleSignPositive = operation > 0;
-
-            // { // DEBUG ///////////////////
-            //     Debug.Log($"Angle {pointI} {pointJ} - op {operation} - positive {angleSignPositive}");
-            //     Debug.DrawLine(pointI, pointI, Color.magenta, 0.1f);
-            //     GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = pointI;
-            // } // DEBUG ///////////////////
-
-            allPositive &= angleSignPositive;
-            allNegative &= !angleSignPositive;
-        }
-
-        return allPositive || allNegative;
+        return result;
     }
 }
