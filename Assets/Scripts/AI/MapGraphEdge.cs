ad7c0eb99e7debe8b7326547ab66052e81c112c8
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-02 15:51:07 -0300

    🔨Add distance property on MapGraphEdge

--- Assets/Scripts/AI/MapGraphEdge.cs
+++ Assets/Scripts/AI/MapGraphEdge.cs
@@ -7,9 +7,13 @@ public class MapGraphEdge
     public GameObject PointA {get; protected set;}
     public GameObject PointB {get; protected set;}
 
+    public float Distance {get; protected set;}
+
     public MapGraphEdge(GameObject pointA, GameObject pointB)
     {
         this.PointA = pointA;
         this.PointB = pointB;
+
+        Distance = Vector3.Distance(pointA.transform.position, pointB.transform.position);
     }
 }
