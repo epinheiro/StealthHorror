6ac3a0c256d8c19a69283555d749bb4f0b3ce9d8
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-04 12:03:33 -0300

    ✨Add ShuffledNeighbors in MapGraphNode

--- Assets/Scripts/AI/MapGraphNode.cs
+++ Assets/Scripts/AI/MapGraphNode.cs
@@ -6,13 +6,14 @@ public class MapGraphNode
 {
     public GameObject Vertex {get; protected set;}
     public NodeType type {get; protected set;}
-    public HashSet<GameObject> Neighbors;
+    public List<GameObject> Neighbors;
+    public List<GameObject> ShuffledNeighbors => Shuffle(Neighbors);
 
     private void ConstructorBase(GameObject vertex)
     {
         this.Vertex = vertex;
         type = (NodeType)Enum.Parse(typeof(NodeType), vertex.tag, true);
-        Neighbors = new HashSet<GameObject>();
+        Neighbors = new List<GameObject>();
     }
 
     public MapGraphNode(GameObject vertex)
@@ -31,8 +32,27 @@ public class MapGraphNode
         return Neighbors.Contains(adjacent);
     }
 
-    public bool InsertNeighbors(GameObject adjacent)
+    public void InsertNeighbors(GameObject adjacent)
     {
-        return Neighbors.Add(adjacent);
+        Neighbors.Add(adjacent);
+    }
+
+    private System.Random rng = new System.Random(DateTime.Now.Second);
+
+    private List<T> Shuffle<T>(List<T> list)
+    {
+        T[] shuffled = new T[list.Count];
+        list.CopyTo(shuffled);
+
+        int n = shuffled.Length;
+        while (n > 1) {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = shuffled[k];
+            shuffled[k] = shuffled[n];
+            shuffled[n] = value;
+        }
+
+        return new List<T>(shuffled);
     }
 }
