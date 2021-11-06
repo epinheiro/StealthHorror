4779249c8412c4ddba3277a5460c08befbfe46d1
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-06 17:00:03 -0300

    Revert "🔧Make SoundCommunicationLayer fetch all objects with SoundListener"
    This reverts commit 3b62bd04d4daf75c8c1f4ac7ee935e3f895e671d.

--- Assets/Scripts/SystemicLayer/SoundCommunicationLayer.cs
+++ Assets/Scripts/SystemicLayer/SoundCommunicationLayer.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class SoundCommunicationLayer : MonoBehaviour
 {
-    List<SoundListener> listeners;
+    [SerializeField] SoundListener[] listeners;
 
     public static SoundCommunicationLayer instance;
 
     void Awake()
     {
         instance = this;
-
-        GameObject[] goArray = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
-        foreach(GameObject go in goArray)
-        {
-            SoundListener listener = go.GetComponent<SoundListener>();
-            if(listener != null)
-                listeners.Add(listener);
-        }
     }
 
     public void MakeSound(SoundType soundType, Vector3 position, Room room)
