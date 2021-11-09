c1ee24bc1dc45cefa501079e3fe34fadfe4a9dcc
epinheiro <edu.g.pinheiro@gmail.com>
2021-11-09 10:28:43 -0300

    ♻️✨Add controllers access in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,14 @@ public class GameManager : MonoBehaviour
 
     public DebugSettings DebugSettings;
 
+    public PlayerController Player {get; protected set;}
+    public MonsterController Monster {get; protected set;}
+
     void Awake()
     {
         Instance = this;
         DebugSettings = Resources.Load<DebugSettings>("DebugSettings");
+        Player = GameObject.Find("Player").GetComponent<PlayerController>();
+        Monster = GameObject.Find("Monster").GetComponent<MonsterController>();
     }
 }
